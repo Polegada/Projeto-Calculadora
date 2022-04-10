56a62cfd4cc428d99ea2162a60e150da588b7299
Michael Fernandes <devmkz7@gmail.com>
2022-04-10 19:40:21 -0300

    java script setado com o eval

--- JS/calculadora.js
+++ JS/calculadora.js
@@ -1,3 +1,15 @@
-function insert() {
-  document.getElementById('resultado').innerHTML;
+function insert(num) {
+  var numero = document.getElementById('resultados').innerHTML;
+  document.getElementById('resultados').innerHTML = numero + num;
+}
+
+function clean() {
+  document.getElementById('resultados').innerHTML = '';
+}
+
+function calcular() {
+  var resultado = document.getElementById('resultados').innerHTML;
+  if (resultado) {
+    document.getElementById('resultados').innerHTML = eval(resultado);
+  }
 }
